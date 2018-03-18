b047567134f1a153e3d946ae2c44c771da20291c
Dave Shepherd <david.shepherd@wealthwizards.com>
2018-03-18 16:12:42 +0000

    Tidy up and work with circleci

--- build.sh
+++ build.sh
@@ -1,11 +1,8 @@
 #! /bin/bash
 
-# Ensure we use the containing directory, irrelevant of where this script is called from
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
 PACKER=$(which packer)
 [[ -z ${PACKER} ]] &&  echo 'packer command not found in the search path. exiting...' && exit 1
 
-source ${DIR}/../environments/config.env
+VAULT_VERSION=0.9.5
 
-${PACKER} build -var "london_vpc_id=${LONDON_VPC}" -var "london_subnet_id=${LONDON_SUBNET}" -var "ireland_vpc_id=${IRELAND_VPC}" -var "ireland_subnet_id=${IRELAND_SUBNET}" -var "vault_version=0.9.5" vault.json
+${PACKER} build -var "london_vpc_id=${LONDON_VPC}" -var "london_subnet_id=${LONDON_SUBNET}" -var "ireland_vpc_id=${IRELAND_VPC}" -var "ireland_subnet_id=${IRELAND_SUBNET}" -var "vault_version=${VAULT_VERSION}" vault.json
