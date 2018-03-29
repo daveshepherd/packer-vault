982b162e076edea023cd2111a7cd59a1b05bc4e1
Dave Shepherd <david.shepherd@wealthwizards.com>
2018-03-29 15:52:04 +0100

    Update to Vault 0.9.6

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 PACKER=$(which packer)
 [[ -z ${PACKER} ]] &&  echo 'packer command not found in the search path. exiting...' && exit 1
 
-VAULT_VERSION=0.9.5
+VAULT_VERSION=0.9.6
 
 ${PACKER} build -var "london_vpc_id=${LONDON_VPC}" -var "london_subnet_id=${LONDON_SUBNET}" -var "ireland_vpc_id=${IRELAND_VPC}" -var "ireland_subnet_id=${IRELAND_SUBNET}" -var "vault_version=${VAULT_VERSION}" vault.json
