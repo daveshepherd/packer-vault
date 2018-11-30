32df734c7cd281d73fffe948bf8e48028dd82f2f
Dave Shepherd <david.shepherd@wealthwizards.com>
2018-11-30 16:10:31 +0000

    Update to vault 0.11.5

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 PACKER=$(which packer)
 [[ -z ${PACKER} ]] &&  echo 'packer command not found in the search path. exiting...' && exit 1
 
-VAULT_VERSION=0.10.4
+VAULT_VERSION=0.11.5
 
 ${PACKER} build -var "region=${REGION}" -var "vpc_id=${VPC_ID}" -var "subnet_id=${SUBNET_ID}" -var "vault_version=${VAULT_VERSION}" -var "destination_regions=${DESTINATION_REGIONS}" build.json
